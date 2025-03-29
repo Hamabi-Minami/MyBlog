0eb7349b7cba02bb6690bdcfccda3fd9d7767d27
dingzhen <zhishi-xuebao@outlook.com>
2025-03-28 21:52:42 -0600

    add auth api

---
+++ Backend/app/schemas/auth.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+
+
+class LoginRequest(BaseModel):
+    account: str = Field(..., min_length=4, max_length=50)
+    password: str = Field(..., min_length=6, max_length=128)
+
+class LoginResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+    expires_in: int
+
